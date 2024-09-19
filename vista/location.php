5069327dc2a1205d2a0363bb40f41ca395f5b8b4
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-09-18 21:55:57 -0600

    Update location.php

--- vista/location.php
+++ vista/location.php
@@ -9,9 +9,7 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="../assets/css/estilos.css">
-    <link rel="stylesheet" href="../assets/css/location.css">
     <link rel="stylesheet" href="../assets/css/footer.css">
-    <link rel="stylesheet" href="../assets/css/location.js">
     
 
 
@@ -21,7 +19,7 @@
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img height="120" src="/Proyecto Web/imagenes/descargar-removebg-preview.png" alt=""></a>
+                <img height="120" src="/Proyecto Web/imagenes/logo.jpeg" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -65,9 +63,13 @@
             <nav class="navbar navbar-expand-lg navbar-light bg-light">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="navbar-brand" href="#">
-                            <img height="25" src="/Proyecto Web/imagenes/carrito.png" alt="">Carrito(0)</a>
-                    </li>
+                    <li class="nav-item active">
+                        <a class="btn btn-primary btn-sm me-2" href="../vista/carrito.php">Carrito(<?php
+                        echo (empty($_SESSION['carrito']))?0:count($_SESSION['carrito']);
+                        ?>)</a>
+                        <a href="login.php" class="btn btn-success btn-sm"><i class="fas fa-user"></i> Ingresar</a>
+
+                            </li>
                 </ul>
             </nav>
         </nav>
@@ -172,7 +174,7 @@
                                         <a href="#">Servicios</a>
                                     </li>
                                     <li>
-                                        <a href="#">Contactanos</a>
+                                        <a href="../vista/contacto.php">Contactanos</a>
                                     </li>
 
                                 </ul>
