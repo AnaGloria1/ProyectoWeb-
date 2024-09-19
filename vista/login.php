e5465b5368cfecb1a381de3826877212089c441e
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-09-18 21:55:21 -0600

    Update login.php

--- vista/login.php
+++ vista/login.php
@@ -38,7 +38,7 @@ if (isset($_SESSION["usuario"])) {
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img height="120" src="../imagenes/descargar-removebg-preview.png" alt=""></a>
+                <img height="120" src="../imagenes/logo.jpeg" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
