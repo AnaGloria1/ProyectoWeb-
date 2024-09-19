483a61cde56c7c8f5dc9e1f6299b16ce74488430
AnaGloria1 <145166017+AnaGloria1@users.noreply.github.com>
2024-09-18 21:39:24 -0600

    Update carritoA.php

--- admin/carritoA.php
+++ admin/carritoA.php
@@ -4,7 +4,7 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-require_once('../modelos/conexion.php');
+require_once('../controladores/conexion.php');
 $mensaje = "";
 function obtenerUnidadesEnStock($productoID, $pdo) {
     // Preparar una consulta SQL para obtener las Unidades en Stock del producto con ID $productoID.
