5a668d4091fe568e13d09ef2621864a95599d311
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-09-18 21:38:48 -0600

    Update medicamentos.php

--- usuario/medicamentos.php
+++ usuario/medicamentos.php
@@ -1,3 +1,4 @@
+Se usó un 79% del almacenamiento … Si te quedas sin almacenamiento, no podrás crear, editar ni subir archivos. Obtén 100 GB de almacenamiento por $34.00 MXN 0 durante 1 mes.
 <?php
 session_start();
 if (!isset($_SESSION["usuario"])){
@@ -52,7 +53,7 @@ include './carritoU.php';
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img height="120" src="/Proyecto Web/imagenes/descargar-removebg-preview.png" alt=""></a>
+                <img height="120" src="/Proyecto Web/imagenes/logo.jpeg" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
