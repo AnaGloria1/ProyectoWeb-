0b367186d94cbec30bf27347356821b70e67ee93
AnaGloria1 <145166017+AnaGloria1@users.noreply.github.com>
2024-09-18 20:57:46 -0600

    Update mostrarcarritoA.php

--- admin/mostrarcarritoA.php
+++ admin/mostrarcarritoA.php
@@ -38,7 +38,7 @@ include './carritoA.php';
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img height="120" src="/Proyecto Web/imagenes/descargar-removebg-preview.png" alt=""></a>
+                <img height="120" src="/Proyecto Web/imagenes/logo.jpeg" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
