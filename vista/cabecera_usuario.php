cfc816bae8b9b9f50f8392c2b692777b8b351635
Heriberto <106274607+HeryLB@users.noreply.github.com>
2024-09-18 21:58:48 -0600

    Update cabecera_usuario.php

--- vista/cabecera_usuario.php
+++ vista/cabecera_usuario.php
@@ -7,8 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css"
         integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" />
-    <link rel="stylesheet" href="./assets/css/estilos.css">
-    <link rel="stylesheet" href="./assets/css/footer.css">
+    <link rel="stylesheet" href="../assets/css/estilos.css">
+    <link rel="stylesheet" href="../assets/css/footer.css">
     <link rel="stylesheet" href="./assets/css/cuidadopersonal.css">
     <link rel="stylesheet" href="./assets/css/contacto.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.7/css/swiper.min.css">
@@ -32,14 +32,15 @@
     <!--carrito-->
 
 
-    <title>Farmacia La Divina Providencia</title>
+    <title>HAC</title>
 </head>
+
 
 <body>
     <header>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <a class="navbar-brand" href="#">
-                <img height="120" src="/Proyecto Web/imagenes/descargar-removebg-preview.png" alt=""></a>
+                <img height="120" src="/Proyecto Web/imagenes/logo.jpeg" alt=""></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -48,7 +49,7 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item active">
-                        <a class="nav-link" href="../index.php">Inicio<span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="../index_usuario.php">Inicio<span class="sr-only">(current)</span></a>
                     </li>
                     <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown"
@@ -88,7 +89,7 @@
                         ?>)</a>
                             </li>
                             <li class="nav-item">
-              <a class="nav-link" href="../vista/miperfil.php">Mi perfil</a>
+              <a class="nav-link" href="../usuario/miperfil.php">Mi perfil</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="../vista/cerrarseccion.php">cerrar sesion</a>
